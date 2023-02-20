d32b1c187edb4b0a17c8e0daeea037c4a48ee1c4
Maxim Ichetovkin <ichetovkin.maxim@gmail.com>
2023-02-20 23:36:01 +0800

    feat: добавить логику для NavigationPopup

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,7 @@
+/* eslint-disable react/jsx-no-bind */
 import { Link } from 'react-router-dom';
+
+import { useState } from 'react';
 
 import NavTab from './NavTab';
 import Navigation from './Navigation';
@@ -8,14 +11,19 @@ import logo from '../images/logo.svg';
 
 function Header() {
     const isLanding = !true; // TODO
-    const menuIsOpened = !true; // TODO
+
+    const [menuIsOpened, setMenuIsOpened] = useState(false);
+
+    function handleMenuClick() {
+        setMenuIsOpened(!menuIsOpened);
+    }
 
     return (
         <header className="header">
             <Link to="/"><img className="header__logo" src={logo} alt="логотип" /></Link>
             {isLanding ? <NavTab /> : <Navigation />}
-            {isLanding || (<button className={menuIsOpened ? 'header__burger-menu_active' : 'header__burger-menu'} type="button" aria-label="Burger-menu" />)}
-            {menuIsOpened && <NavigationPopup />}
+            {isLanding || (<button className={menuIsOpened ? 'header__burger-menu_active' : 'header__burger-menu'} type="button" aria-label="Burger-menu" onClick={handleMenuClick} />)}
+            {menuIsOpened && <NavigationPopup onClose={handleMenuClick} />}
         </header>
     );
 }
--- src/components/NavigationPopup.js
+++ src/components/NavigationPopup.js
@@ -1,26 +1,25 @@
+/* eslint-disable react/prop-types */
 import { NavLink } from 'react-router-dom';
 import iconMain from '../images/icon-main.svg';
 
-export default function NavigationPopup() {
+export default function NavigationPopup({ onClose }) {
     const activeStyle = { // Для стилей активной ссылки NavLink
         borderBottom: '1px solid',
     };
 
-    const menuIsOpened = !true;
-
     return (
-        <div className={menuIsOpened ? 'navigation-popup' : 'navigation-popup_closed'}>
+        <div className="navigation-popup">
             <div className="navigation-popup__cover" />
             <div className="navigation-popup__container">
-                <button className="navigation-popup__close-button" type="button" aria-label="Close-icon" /* onClick={ TODO} */ />
+                <button className="navigation-popup__close-button" type="button" aria-label="Close-icon" onClick={onClose} />
                 <nav className="navigation-popup__links-container">
                     <div className="navigation__films navigation__films_popup">
-                        <NavLink to="/" className="navigation-popup__homepage" style={({ isActive }) => (isActive ? activeStyle : undefined)}>Главная</NavLink>
-                        <NavLink to="/movies" className="navigation__movies navigation__movies_popup" style={({ isActive }) => (isActive ? activeStyle : undefined)}>Фильмы</NavLink>
-                        <NavLink to="/saved-movies" className="navigation__movies navigation__movies_popup" style={({ isActive }) => (isActive ? activeStyle : undefined)}>Сохраненные фильмы</NavLink>
+                        <NavLink to="/" className="navigation-popup__homepage" style={({ isActive }) => (isActive ? activeStyle : undefined)} onClick={onClose}>Главная</NavLink>
+                        <NavLink to="/movies" className="navigation__movies navigation__movies_popup" style={({ isActive }) => (isActive ? activeStyle : undefined)} onClick={onClose}>Фильмы</NavLink>
+                        <NavLink to="/saved-movies" className="navigation__movies navigation__movies_popup" style={({ isActive }) => (isActive ? activeStyle : undefined)} onClick={onClose}>Сохраненные фильмы</NavLink>
                     </div>
                     <div className="navigation__account navigation__account_popup">
-                        <NavLink to="/profile" className="navigation__account-link navigation__account-link_popup" style={({ isActive }) => (isActive ? activeStyle : undefined)}>Аккаунт</NavLink>
+                        <NavLink to="/profile" className="navigation__account-link navigation__account-link_popup" style={({ isActive }) => (isActive ? activeStyle : undefined)} onClick={onClose}>Аккаунт</NavLink>
                         <img className="navigation__account-icon" src={iconMain} alt="иконка аккаунта" />
                     </div>
                 </nav>
