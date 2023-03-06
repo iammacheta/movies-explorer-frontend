b3ceb3ab0bbc92f789818b6282d70a820b5a4fcb
Maxim Ichetovkin <ichetovkin.maxim@gmail.com>
2023-03-06 14:05:50 +0800

    feat: добавить кастомную валидацию сабмита формы поиска

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,13 +1,47 @@
 import FilterCheckbox from './FilterCheckbox';
+import { useEffect, useState } from 'react';
 
 export default function SearchForm({ onShorts, shortsIsChecked, onFindClick, searchKey, onInputChange }) {
+
+    const [serchFormInput, setSerchFormInput] = useState(null);
+    const [emailError, setEmailError] = useState(null);
+
+    function handleSubmit(e) {
+        if (!serchFormInput.validity.valid) {
+            showError();
+            e.preventDefault();
+            return
+        }
+
+        onFindClick(e);
+    }
+
+    function showError() {
+        if (serchFormInput.validity.valueMissing) {
+            emailError.textContent = "Нужно ввести ключевое слово";
+        } else {
+            emailError.textContent = "Произошла ошибка. Попробуйте еще раз";
+        }
+        emailError.className = "searchForm__error searchForm__error_active";
+    }
+
+    useEffect(() => {
+        const serchFormInput = document.querySelector(".search-form__input");
+        const emailError = document.querySelector(".searchForm__error");
+
+        emailError.textContent = '';
+        setSerchFormInput(serchFormInput);
+        setEmailError(emailError);
+        emailError.className = "searchForm__error";
+    });
 
     return (
         <div className="search-form">
             <form
                 className="search-form__container"
                 name='searchForm'
-                onSubmit={onFindClick}
+                onSubmit={handleSubmit}
+                noValidate
             >
                 <input
                     className="search-form__input"
@@ -20,6 +54,7 @@ export default function SearchForm({ onShorts, shortsIsChecked, onFindClick, sea
                 />
                 <button className="search-form__button" type="submit">Найти</button>
             </form>
+            <span className="searchForm__error"></span>
             <FilterCheckbox onShorts={onShorts} shortsIsChecked={shortsIsChecked} />
         </div>
     );
